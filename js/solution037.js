be78e5c87c5a72276dbd0acec06dbd874c398409
kalen <yokalen@gmail.com>
2022-02-05 21:39:49 -0800

    Swap values

---
+++ js/solution037.js
@@ -0,0 +1,15 @@
+//param
+//array with two elements
+
+//return 
+//array with values swapped
+
+//example
+//[1,2] => [2,1]
+
+//psuedo code
+//reverse the array and return
+
+function swapValues(arr) {
+    return arr.reverse();
+}
