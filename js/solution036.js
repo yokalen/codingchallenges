e9faa6d96f20fe28bee9ba4bc4141b69cf9aeae2
kalen <yokalen@gmail.com>
2022-02-06 21:32:19 -0800

    Binary to decimal

---
+++ js/solution036.js
@@ -0,0 +1,17 @@
+//params
+//binary number given as a string
+
+//return
+//converted binary as a decimal number
+
+//example
+//'1' => 1
+//'1001001' => 73
+
+//psuedo code
+//use a method to convert binary string to decimla number
+//return number
+
+function binToDec(bin){
+  return parseInt(bin, 2)
+}
