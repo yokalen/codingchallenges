21d98381c3e1d567bc0f5e8e03a66ef2627bfca4
kalen <yokalen@gmail.com>
2022-02-12 20:14:42 -0800

    Enumerable magic 25: take the first n elements

---
+++ js/solution031.js
@@ -0,0 +1,9 @@
+//Parameters: array and a number n
+//Return: the first n elements from the array
+//Example: [0, 1, 2, 3, 5, 8, 13], 3) => [0, 1, 2]
+
+//return a piece of the array with the number of elements equal to n
+
+function take(arr, n) {
+  // Your code here
+}
