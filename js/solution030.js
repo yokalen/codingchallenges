a921f0b1b11098270645bb80552088fe6ff35620
kalen <yokalen@gmail.com>
2022-02-14 20:06:56 -0800

    Grasshopper: basic function fixer

---
+++ js/solution030.js
@@ -0,0 +1,13 @@
+//Parameter: any whole number, positive, zero, or negative
+//Return: the number passed in plus 5
+//Example: 5 => 10
+//          10 => 15
+//          0 => 5
+//          -5 => 0
+
+//take in a number, add five, and return the new number
+
+function addFive(num) {
+  let total = num + 5
+  return total
+}
