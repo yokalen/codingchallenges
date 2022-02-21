2893d24ff20edc78b08900e891ae04b71db1f928
kalen <yokalen@gmail.com>
2022-02-20 19:40:41 -0800

    Check for factor

---
+++ js/solution014.js
@@ -0,0 +1,11 @@
+//P: base, factor
+//R: true or false - is factor a factor of base
+//E: 6, 3 => true
+//   6, 2 => true
+//   7, 2 => false
+//   1, 2 => false
+//P: check if base mod factor is 0
+//   if yes return true, otherwise false
+function checkForFactor (base, factor) {
+  return base % factor === 0
+}
