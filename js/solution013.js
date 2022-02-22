b7edbb76d7b217eb821f508ef40d86bf4e973c5a
kalen <yokalen@gmail.com>
2022-02-21 19:31:23 -0800

    Is it even?

---
+++ js/solution013.js
@@ -0,0 +1,12 @@
+//Parameters: num, float or integer, positive or negative
+//Return: true if even num or false
+//Examples: 4 => true
+//          5 => false
+//          -2 => true
+//          4.2 => false
+//Psuedo code
+//return true if num is divisible by two
+//otherwise return false
+function testEven(n) {
+    return n % 2 === 0;
+}
