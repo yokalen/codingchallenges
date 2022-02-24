700873a968a8401e4f6c646a29270c7901e9974c
kalen <yokalen@gmail.com>
2022-02-24 12:39:20 -0800

    Switch it up

---
+++ js/solution009.js
@@ -0,0 +1,17 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+// Output :: "One".
+
+//P: num 0-9
+//R: word representing the num
+//E: 0 => 'Zero'
+//   4 => 'Four'
+//   7 => 'Seven'
+//   2 => 'Two'
+//P: 
+// whatever number is provided as a parameter, return it as a string word
+
+
+
+
