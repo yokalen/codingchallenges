b23199a4c6892242bccacad7745a9ea772824d92
kalen <yokalen@gmail.com>
2022-02-28 23:12:52 -0800

    Twice as old

---
+++ js/solution006.js
@@ -0,0 +1,12 @@
+//PREP
+
+//Parameters: current age of father and current age of his son in years, whole positive numbers
+//Return: how many years ago or in how many years father will be twice as old as his son. answer = (2 * sAge - father's age) answer is always positive
+//Example: 36, 7 => 14
+//         55,30 => 5
+//         42,21 => 0
+//         22,1 => 20
+//Psudo code
+//double son's age and subtract father's
+//if the answer is a negative number make it positive
+//return answer
