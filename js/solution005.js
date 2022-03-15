7293ff04ac57c11f955e9e84a7fac91bc7a09375
kalen <yokalen@gmail.com>
2022-03-14 23:57:05 -0700

    Messi goals function

---
+++ js/solution005.js
@@ -0,0 +1,12 @@
+//PREP
+
+//Parameters: three whole numbers representing goals in three leagues
+//Return: sum of three numbers represented total number of goals
+//Example: 5,10,2 => 17
+//          0,0,1 => 1
+//          10,12,27 => 49
+//          20, 12, 9 => 41
+//Psuedo code: 
+//take in three numbers
+//sum numbers
+//return total
