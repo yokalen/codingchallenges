f02b82dc76384ce3f12b18635f34c8eaa7b9ea94
kalen <yokalen@gmail.com>
2022-05-13 23:12:29 -0700

    Set Alarm

---
+++ js/solution003.js
@@ -0,0 +1,8 @@
+//PREP
+
+//Parameters: two parameters, first is employed, true whenever you are employed and second is vacation, true whenever you are on vacation
+//Return: True if you are employed and not on vacation, otherwise false
+//Example: true, false => false
+//          false, true => false
+//          false, false => false
+//          true, false => true
