fdc3080ed44f0c90ad14c158b0bd3146682c50b6
kalen <yokalen@gmail.com>
2022-05-23 17:01:04 -0700

    Keep up the hoop

--- js/solution011.js
+++ js/solution011.js
@@ -1,7 +1,9 @@
-function hoopCount (n) {
-   if(n < 10){
-     return 'Keep at it until you get it' 
-   }else {
-     return 'Great, now move on to tricks'
-   }
-}
+// function hoopCount (n) {
+//    if(n < 10){
+//      return 'Keep at it until you get it' 
+//    }else {
+//      return 'Great, now move on to tricks'
+//    }
+// }
+
+const hoopCount = n => n < 10 ? 'Keep at it until you get it' : 'Great, now move on to tricks'
