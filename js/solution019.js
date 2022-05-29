4b2c770048094df8e44960d3a774bf084e435134
kalen <yokalen@gmail.com>
2022-05-29 01:47:54 -0700

    Sort and star

--- js/solution019.js
+++ js/solution019.js
@@ -1,9 +1,3 @@
-// You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
-
-// The returned value must be a string, and have "***" between each of its letters.
-
-// You should not remove or add elements from/to the array
-
 //P: array of strings
 //R: the first value in the array after it has been sorted alphabetically with *** between each letter
 //E: ['cat', 'fish', 'dog'] => c***a***t
@@ -12,8 +6,11 @@
 //get the first string in the array
 //return string with *** between each letter
 
-function twoSort(s) {
-    s.sort()
-    let firstWord = s[0];
-    return firstWord.split('').join('***')
-}
+// function twoSort(s) {
+//     s.sort()
+//     let firstWord = s[0];
+//     return firstWord.split('').join('***')
+// }
+
+//refactored
+const twoSort = s => s.sort()[0].split('').join('***')
