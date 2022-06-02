7539e987476b3fd698db517ac82ab4608de18267
kalen <yokalen@gmail.com>
2022-06-01 20:46:18 -0700

    Plural solutions

---
+++ js/solution023.js
@@ -0,0 +1,10 @@
+//P: number
+//R: true or false if number is 1
+//E: 5 => true
+//   14 => true
+//   1 => false
+//P:
+//check to see if number = 1
+//return true or false
+
+const plural = n => n !== 1 
