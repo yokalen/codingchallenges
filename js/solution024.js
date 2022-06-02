6c242e75d16427136098c1eb9af904155086b574
kalen <yokalen@gmail.com>
2022-06-01 20:57:35 -0700

    Get character from ASCII value

---
+++ js/solution024.js
@@ -0,0 +1,8 @@
+//P: number
+//R: corresponding ASCII char for that value
+//E: 65 => 'A'
+//P: return ASCII code that corresponds to number
+
+function getChar(c){
+  return String.fromCharCode(c);
+}
