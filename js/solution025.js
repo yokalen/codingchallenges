1edc62db6a431e619ebed9925dbe0dd44c7019fd
kalen <yokalen@gmail.com>
2022-06-03 21:27:50 -0700

    Vacation Bottle  Discount

---
+++ js/solution025.js
@@ -0,0 +1,11 @@
+//P: high price, discount, cost of vacation
+//R: number of bottles you have to buy at a discount that the savings over the high price will cover the cost of vacation
+//E: 10, 10%, 500 => 500
+//   20, 8%, 800 => 500
+//   15, 12%, 900 => 500
+//   10, 20%, 600 => 300
+//P: 
+// return cost of vacation divided by the high price times the discount
+function dutyFree(price, discount, vacation){
+    return Math.floor( vacation / (price * (discount/100) ) );
+}
