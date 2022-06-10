fc6a6ff65f6c80b0e2990ba1455878567e8f96a3
kalen <yokalen@gmail.com>
2022-06-09 19:02:03 -0700

    Surface area and volume of a box

--- js/solution034.js
+++ js/solution034.js
@@ -12,9 +12,11 @@
 // v = width * height * depth
 // return [a, v]
 
-function getSize(width, height, depth){
-    let a = 2 * width * height + 2 * height * depth + 2 * depth * width
-    let v = width * height * depth
-    let arr = [a, v]
-    return arr;
-}
+// function getSize(width, height, depth){
+//     let a = 2 * width * height + 2 * height * depth + 2 * depth * width
+//     let v = width * height * depth
+//     let arr = [a, v]
+//     return arr;
+// }
+
+const getSize = (width, height, depth) => [2 * width * height + 2 * height * depth + 2 * depth * width, width * height * depth]
