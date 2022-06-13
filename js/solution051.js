1cb88a5c9d09f27657d74c5d12f759c871c77de9
My Computer <yokalen@gmail.com>
2022-06-12 23:22:47 -0700

    Pixel art planning

---
+++ js/solution051.js
@@ -0,0 +1,9 @@
+//params
+// wall length and pixel length in mm
+
+//return true if the pixel art will fit perfectly on the wall, ie if wall size / pixel size = 0
+
+//example
+//4050, 27 => true
+//4066, 27 => false
+
