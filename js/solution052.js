de37db83d0bbc046cfd5f26d2594dbf236636875
My Computer <yokalen@gmail.com>
2022-06-15 23:47:58 -0700

    Find the position

--- js/solution052.js
+++ js/solution052.js
@@ -8,3 +8,8 @@
 //a => 1
 //z => 26
 //e => 5
+
+function position(letter){
+    let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    return alphabet.indexOf(letter) + 1;
+}
