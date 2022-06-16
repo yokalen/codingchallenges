12bdcaf90de7a116ee8d6cfda11c161eac928e74
My Computer <yokalen@gmail.com>
2022-06-15 22:15:28 -0700

    Is it a number?

---
+++ js/solution053.js
@@ -0,0 +1,17 @@
+//params
+//a string s
+//return
+//true if it's a valid string integer or floating number, otherwise false
+//examples
+//"3" => true
+//"-3.23" => true
+//"3-4" => false
+//"zero" => false
+
+function isDigit(s) {
+    if(Number(s)){
+      return true
+    }else{
+      return false
+    }
+  }
