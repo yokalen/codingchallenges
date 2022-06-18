b411c8fb5490b3a47a223b8ba8e69dbd5923bcdf
My Computer <yokalen@gmail.com>
2022-06-17 22:31:36 -0700

    Multiple of index

---
+++ js/solution050.js
@@ -0,0 +1,17 @@
+//params
+//an array of numbers
+//return
+//a new array of elements which are multiple of their own index in the array parameter
+//examples
+//[22,-6,32,82,9,25] => [-6,32,25]
+//[68,-1,1,-7,10,10] => [-1,10]
+//[11, -11] => [-11]
+
+function multipleOfIndex(array) {
+    let result = [];
+    for(let i = 0; i < array.length; i++){
+      if(array[i] % i === 0){
+        result.push(array[i])
+    }
+    return result;
+  }
