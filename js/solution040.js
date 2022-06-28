e3d996f76438a3c89d7d06dd89cfc4f1055f23a4
My Computer <yokalen@gmail.com>
2022-06-27 20:38:57 -0700

    Printing array elements with comma delimiters

---
+++ js/solution040.js
@@ -0,0 +1,16 @@
+//P
+//array of elements
+
+//R 
+//string with commas separating elements of the array in the same order
+
+//E
+//['h', 'o', 'l', 'a'] => 'h,o,l,a'
+
+//P
+//join elements in array with , as seperator
+
+
+function printArray(array){
+    return array.join(',')
+  }
