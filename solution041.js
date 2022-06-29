a125c645398d5001e2cdf2f0664fcfcdb2182d6e
My Computer <yokalen@gmail.com>
2022-06-29 12:52:47 -0700

    Determine offspring sex based on genes

---
+++ solution041.js
@@ -0,0 +1,6 @@
+//p
+//two-character string that starts is X and ends with either X or Y
+//r
+//if the last letter is X return "Congratulations! You're going to have a daughter." if it is Y return "Congratulations! You're going to have a son."
+
+const chromosomeCheck = sperm => sperm[1] === 'X' ? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son."
