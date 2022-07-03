14b267b0e87628007d2ce0590d5ceaacf0fb547e
My Computer <yokalen@gmail.com>
2022-07-02 17:59:21 -0700

    How old will I be in 2099?

---
+++ js/solution043.js
@@ -0,0 +1,14 @@
+//p
+//two dates, one is the date you were born and the other is the date used to calculate how old you will be at that time
+//r
+//for dates in the future "You are (number) years old" and for dates in the past "You will be born in (number) years" and if the birth year equals the request year "You were born this very year"
+//e
+//2012, 2016 => "You are 4 years old."
+//1989, 2016 => "You are 27 years old."
+//2000, 2090 => "You are 90 years old."
+//2000, 1990 => "You will be born in 10 years."
+//year of request - year of birth
+//if the number is zero
+//if the number is negative
+//else the number is positive
+
