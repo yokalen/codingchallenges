a9f019ae095b9de11c41a395ba4627c2a56a90b9
My Computer <yokalen@gmail.com>
2022-07-11 12:42:15 -0700

    Sum of differences in array

--- js/solution047.js
+++ js/solution047.js
@@ -7,3 +7,11 @@
 //e
 //[10, 2, 1] => (10 - 2) + (2 - 1) = 8 + 1 = 9
 
+function sumOfDifferences(arr) {
+    let sum = 0;
+    let sortedArr = arr.sort((a, b) => b - a);
+    for(let i = 0; i < sortedArr.length - 1; i++){
+        sum += sortedArr[i] - sortedArr[i + 1];
+    }
+    return sum;
+}
