264bc6b20af4b9aa57737b0fce0a926e8bc162bc
My Computer <yokalen@gmail.com>
2022-07-21 21:48:42 -0700

    Disemvowel Trolls

---
+++ js/solution060.js
@@ -0,0 +1,12 @@
+//params
+//a string of words
+
+//return
+//a new string with all vowels removed
+
+//example
+//"This website is for losers LOL!" => "Ths wbst s fr lsrs LL!"
+
+//psuedo code
+//split the string at each vowel and then recombine 
+
