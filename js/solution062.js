f761916479f149d90522d6c29e00422341f64215
My Computer <yokalen@gmail.com>
2022-07-23 16:01:23 -0700

    Did I get a bonus?

---
+++ js/solution062.js
@@ -0,0 +1,17 @@
+//params
+//salary, bonus
+//salary is an integer and bonus is a boolean
+
+//return
+//if bonus is true, multiply salary by 10 
+//if bonus is false, return salary
+//return total figure as a string prefixed with "£" (= "\u00A3")
+
+//example
+//10000, true => '£100000'
+//25000, true => '£250000'
+//10000, false => '£10000'
+
+//psuedo code
+//calculate the total number based on if bonus is true or false
+//return a string with the prefix followed by the number
