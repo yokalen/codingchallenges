94c62e7a65efd8bedc7247c2d1450fb60376918d
My Computer <yokalen@gmail.com>
2022-07-27 16:22:05 -0700

    Stringy strings

---
+++ js/solution063.js
@@ -0,0 +1,15 @@
+//params
+//size, always a positive whole number
+
+//return
+//a string of alternating '1' and '0' that has a length of size
+
+//example
+//6 => '101010'
+//4 => '1010'
+//12 => '101010101010'
+
+//psuedo code
+//if size % 2 = 0, return '10' size/2 number of times
+//if size % 2 != 0, return '10' size/2 number of times + 1
+
