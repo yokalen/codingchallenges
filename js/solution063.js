526c40847a36189e9927b3e26491d2839caad1e5
My Computer <yokalen@gmail.com>
2022-07-28 16:24:23 -0700

    Stringy strings

--- js/solution063.js
+++ js/solution063.js
@@ -13,3 +13,10 @@
 //if size % 2 = 0, return '10' size/2 number of times
 //if size % 2 != 0, return '10' size/2 number of times + 1
 
+function stringy(size) {
+    if(size % 2 === 0){
+        return '10' * (size/2)
+    }else{
+        return '10' * Math.floor(size/2) + '1'
+    }
+}
