9c1da15ceeb00a8ac908c40fa9945995ee417535
My Computer <yokalen@gmail.com>
2022-08-01 14:11:58 -0700

    Jaden Casing Strings

---
+++ js/solution064.js
@@ -0,0 +1,13 @@
+String.prototype.toJadenCase = function (param) {
+    //...
+    let words = param.split(' ')
+    words.forEach(el => {
+        return el[0].toUppercase
+    });
+    return words.join(' ')
+  };
+
+let str = "How can mirrors be real if our eyes aren't real";
+
+console.log(str.toJadenCase);
+  
