3853e1d3b9de9883f5504c904ddc9789b7170ac9
My Computer <yokalen@gmail.com>
2022-08-11 22:07:51 -0700

    Adding Arrays

--- js/solution065.js
+++ js/solution065.js
@@ -29,10 +29,13 @@
 // 
 
 //second try
-// function arrAdder(arr) {
-//     let result = '[]'
-//     for(let i = 0; i < arr.length; i++){
-//       for(let j = 0; j < arr.length; j++){
-//         result += arr[i][j] + ' '
-//       }
-//       return result
+function arrAdder(arr) {
+    let result = '';
+    for(let i = 0; i < arr[0].length; i++){
+      for(let j = 0; j < arr.length; j++){
+        result += arr[j][i]
+      }
+      result += ' ';
+    }
+    return result.trim()
+}
