6315310f1a1fa8bfe566d6de1b2de7ff0c3b2078
My Computer <yokalen@gmail.com>
2022-08-16 14:10:51 -0700

    Compare within margin

--- js/solution068.js
+++ js/solution068.js
@@ -20,4 +20,15 @@
 //a < b ? -1
 //a > b ? 1
 
-
+function closeCompare(a, b, margin){
+    if(!margin){
+        let margin = 0;
+    }
+    if(Math.abs(a - b) <= margin){
+        return 0;
+    }else if(a < b){
+        return -1;
+    }else{
+        return 1;
+    }
+  }
