b25fd2c97214d25d9a9646ccf62a1f7479044dda
My Computer <yokalen@gmail.com>
2022-08-16 20:23:36 -0700

    Adding values of arrays in a shifted way

---
+++ js/solution069.js
@@ -0,0 +1,15 @@
+//params
+//an array of arrays with whole numbers, the shifting value. the arrays will always have the same number of values, and will never be empty
+
+//return
+//the values of the arrays added into one new array using the shifting value to determine how far over the second array begins adding
+
+//example
+//[[1,2,3,4,5,6], [7,7,7,7,7,-7]], 0 => [8,9,10,11,12,-1]
+//[[1,2,3,4,5,6], [7,7,7,7,7,7]], 3 => [1,2,3,11,12,13,7,7,7]
+
+//psuedo code
+//declare a new array that will be the result
+//add 0s to the beginning of the second array according to the shifting value
+//concat?
+
