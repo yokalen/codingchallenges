aa5134bf735bd647a1ca45d1e8fce2306b9a777a
My Computer <yokalen@gmail.com>
2022-08-19 12:12:44 -0700

    Get the mean of an array

---
+++ js/solution070.js
@@ -0,0 +1,14 @@
+//params
+//an array of numbers
+
+//return the average number of the array rounded down to the nearest integer
+
+//example
+//[2,2,2,2] => 2
+//[1,2,3,4,5] => 3
+
+//psuedo code
+//reduce the array by adding up all the numbers
+//divide by length of the array
+//round down
+
