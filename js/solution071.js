9a42bac231960713c75fb84762a743bf6939f951
My Computer <yokalen@gmail.com>
2022-08-23 19:10:25 -0700

    Find numbers which are divisible by given number

---
+++ js/solution071.js
@@ -0,0 +1,12 @@
+//params
+//an array of numbers and a divisor
+
+//return
+//a new array of all the numbers in the argument array that are divisible by the divisor
+
+//example
+//[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+
+//psudeo code
+//check to see if each number in the array is divisible by the divisor, and if so push it to a new array that is returned
+
