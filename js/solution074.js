6bdb8c789ef9a739de9efe1ac04c8905d5dd3dbb
My Computer <yokalen@gmail.com>
2022-08-29 20:00:56 -0700

    Duck Duck Goose

--- js/solution074.js
+++ js/solution074.js
@@ -9,5 +9,16 @@
 //count the players one by one around in a circle until you reach the given number and get the player's name
 
 //first try
-const duckDuckGoose = (players, goose) => players.length <= goose ? players[(goose - 1)][name] : players[(players.length % goose - 1)];
+// const duckDuckGoose = (players, goose) => players.length <= goose ? players[(goose - 1)][name] : players[(players.length % goose - 1)];
+
+//second
+function duckDuckGoose(players, goose) {
+    let chosen 
+    if(players.length >= goose){
+      chosen = players.length - 1
+    }else{
+      chosen = (goose % players.length) - 1
+    }
+    return players[chosen].name
+  }
 
