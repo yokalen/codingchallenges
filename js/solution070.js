44e39a0095c3c217cc79fcc1c834952ed6f68c66
My Computer <yokalen@gmail.com>
2022-08-31 20:13:28 -0700

    Average array

--- js/solution070.js
+++ js/solution070.js
@@ -13,5 +13,5 @@
 //round down
 
 function getAverage(marks){
-    return Math.floor((marks.reduce((acc, c) => acc + c, 0)) / marks.length)
+    return Math.floor(marks.reduce((acc,c) => acc + c, 0) / marks.length)
   }
