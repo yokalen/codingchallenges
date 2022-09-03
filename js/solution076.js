71f4af7cfac7c64068b2dbdbd58fea065a36e230
My Computer <yokalen@gmail.com>
2022-09-03 11:50:11 -0700

    Filter out the geese

--- js/solution076.js
+++ js/solution076.js
@@ -5,7 +5,5 @@
 
 function gooseFilter (birds) {
     const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-   return birds.filter((el,i)=>{
-    return el != geese[i]
-   })
-  };
+   return birds.filter((el) => !geese.includes(el))
+};
