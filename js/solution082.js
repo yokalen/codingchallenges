458d816fae364da3f09744d4b37009069d5b1026
My Computer <yokalen@gmail.com>
2022-09-12 14:54:01 -0700

    Capitalize sentence

--- js/solution082.js
+++ js/solution082.js
@@ -4,14 +4,26 @@ return the equivalent of the sentence when capitalised. E.g
   capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
 */
 
-function capSentence(text) {
+// first try
+// function capSentence(text) {
+//     //create an array of words
+//      let arr = text.split(' ')
+//      //capitalize the first letter of each word
+//     for(let i = 0; i < arr.length; i++){
+//       let cap = arr[i][0].toUpperCase()
+//       arr[i] = arr[i].replace(arr[i][0], cap)
+//     }
+//     // return as a string
+//     return arr.join(' ')
+//   }
+
+//forEach
+function capSentence(text){
     //create an array of words
-     let arr = text.split(' ')
-     //capitalize the first letter of each word
-    for(let i = 0; i < arr.length; i++){
-      let cap = arr[i][0].toUpperCase()
-      arr[i] = arr[i].replace(arr[i][0], cap)
-    }
-    // return as a string
-    return arr.join(' ')
-  }
+    let words = text.toLowerCase().split(' ')
+    let capWords = []
+
+    //capitalize the first letter of each word
+    words.forEach(word => capWords.push(word[0].toUpperCase() + word.slice(1)))
+    return capWords.join(' ')
+}
