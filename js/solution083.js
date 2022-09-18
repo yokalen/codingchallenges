2c35af1c989ae6c940c0f39e636a85dbe13c52b3
My Computer <yokalen@gmail.com>
2022-09-18 16:21:51 -0700

    Create phone number

--- js/solution083.js
+++ js/solution083.js
@@ -7,7 +7,7 @@
 //[9,2,5,3,1,8,4,7,6,0] => '(925) 318 - 4760'
 
 function phoneNum(arr){
-    return '(' + arr[0] + arr[1] + arr[2] + ') ' + arr[3] + arr[4] + arr[5] + ' - ' + arr[6] + arr[7] + arr[8] + arr[9]
+    return '(' + arr[0] + arr[1] + arr[2] + ') ' + arr[3] + arr[4] + arr[5] + '-' + arr[6] + arr[7] + arr[8] + arr[9]
 }
 
 console.log(phoneNum([4,1,5,2,2,2,2,2,2,2]))
