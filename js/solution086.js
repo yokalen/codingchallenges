1d429f6a00ca5f516cdfcaefce280cb9141d8380
My Computer <yokalen@gmail.com>
2022-09-26 21:50:34 -0700

    Sum array of mixed types

--- js/solution086.js
+++ js/solution086.js
@@ -9,3 +9,7 @@
 function sumMix(arr){
     return arr.reduce((acc, c) => acc + Number(c), 0)
 }
+
+console.log(sumMix([9, 3, '7', '3']), 22)
+console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42)
+console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41)
