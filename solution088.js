4b46b15e5094599650478071e4cd1d5c029a498c
My Computer <yokalen@gmail.com>
2022-10-05 18:55:01 -0700

    Sum unique nums

--- solution088.js
+++ solution088.js
@@ -17,3 +17,7 @@ function repeats(arr){
     }
     return newArr.reduce((acc, c) => acc + c, 0);  
 }
+
+console.log(repeats([4,5,7,5,4,8]),15);
+console.log(repeats([9, 10, 19, 13, 19, 13]),19);
+console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11]),12);
