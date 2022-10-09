49b7804d0dc400715dbd7c7c84e9cea984319eeb
My Computer <yokalen@gmail.com>
2022-10-08 19:23:09 -0700

    Descending order

---
+++ js/solution090.js
@@ -0,0 +1,11 @@
+//param is whole, positive num
+//return num with digits in descending order
+//42145 => 54421
+//145263 => 654321
+//123456789 => 987654321
+
+function descendingOrder(num){
+    //split num into array of nums
+    //sort arr in descending order
+    //join and return
+  }
