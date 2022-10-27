90e93b494052d78f4215cd495616f5b397b4b72e
My Computer <yokalen@gmail.com>
2022-10-26 21:41:18 -0700

    Nth smallest number

--- js/solution094.js
+++ js/solution094.js
@@ -11,3 +11,11 @@
 // nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
 // nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
 // nthSmallest({177,225,243,-169,-12,-5,2,92} ,5) ==> return (92) 
+
+function nthSmallest(arr, n){
+    return arr.sort((a, b) => a - b)[n - 1];
+  }
+  
+  console.log(nthSmallest([3,1,2], 2), 2)
+  console.log(nthSmallest([15,20,7,10,4,3], 3 ), 7)
+  console.log(nthSmallest([177,225,243,-169,-12,-5,2,92], 5), 92)
