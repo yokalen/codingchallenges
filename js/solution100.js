3e2d5483f17fbe513e9f8cf965aea00ddf5b838c
My Computer <yokalen@gmail.com>
2022-11-05 14:42:40 -0700

    Which are in

--- js/solution100.js
+++ js/solution100.js
@@ -4,18 +4,8 @@
 //a1 = ["tarp", "mice", "bull"], a2 = ["lively", "alive", "harp", "sharp", "armstrong"] => []
 
 function inArray(a1,a2){
-    //if each word in a1 is a substring of a word in a2, push that word to a new arry
-    let a3 = []
-    for(let i = 0; i < a1.length; i++){
-        for(let j = 0; j < a2.length; j++){
-            //check for duplicates
-            if(a2[j].search(a1[i])){
-                a3.push(a1[i]) 
-            }
-        }
-    }
-    //sort the new array
-    return a3.sort()
+    let str = a2.join('')
+    return a1.filter(s => str.indexOf(s) !== -1).sort()
   }
 
 console.log(inArray(["arp", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"]), ["arp", "live", "strong"])
