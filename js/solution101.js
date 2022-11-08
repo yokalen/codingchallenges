840f3dcfc6ac0eddf46a4512ae4f776a9d685330
My Computer <yokalen@gmail.com>
2022-11-08 15:58:00 -0800

    Lost number in number sequence

--- js/solution101.js
+++ js/solution101.js
@@ -5,19 +5,18 @@
 //[1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3] => 0
 
 function findDeletedNumber(arr, mixArr) {
-    //sort the mixed arr
-    let mixSorted = mixArr.sort()
     //if arr.length === mixArr.length return 0
-    if(arr.length === mixSorted.length){
+    if(arr.length === mixArr.length){
         return 0;
-    }else{
-    //for loop, if arr[i] !== mixArr[i], return i+1
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] !== mixArr[i]){
-            return i + arr[0];
+    }
+    //for loop
+    for(let num in arr){
+        if(mixArr.indexOf(arr[num]) === -1){
+            return arr[num];
         }
-    }}
-}
+    }
+    return 0;
+    }
 
 console.log(findDeletedNumber([1,2,3,4,5], [3,4,1,5]), 2)
 console.log(findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]), 5)
