16237292c2b4a4627f008f30ae3dd4a4559980cc
My Computer <yokalen@gmail.com>
2022-11-09 14:47:57 -0800

    Array difference

--- js/solution102.js
+++ js/solution102.js
@@ -6,9 +6,7 @@
 //[1,2,2,2,3], [2] => [1,3]
 
 function arrayDiff(a, b) {
-    for(let num in b){
-        return a.filter(el => el !== b[num])
-    }
+    return a.filter(el => !b.includes(el))
 }
 
 console.log(arrayDiff([1,2], [1]), '[2]')
