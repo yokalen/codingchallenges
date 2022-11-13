e7bd6b6c6aa09253685ade1185e806763b1bfee8
My Computer <yokalen@gmail.com>
2022-11-13 12:14:14 -0800

    Find the middle element

--- js/solution103.js
+++ js/solution103.js
@@ -1,17 +1,3 @@
-// You need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
-
-// The input to the function will be an array of three distinct numbers.
-
-// For example:
-
-// gimme([2, 3, 1]) => 0
-// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
-
-// Another example (just to make sure it is clear):
-
-// gimme([5, 10, 14]) => 1
-// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1
-
 //arr of 3 distinct nums
 //index of num that lies between other nums numerically
 //[2,3,1]=>0
