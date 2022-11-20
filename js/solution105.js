3a390680002ba2c15c70f3cf397d5582913e63af
My Computer <yokalen@gmail.com>
2022-11-19 16:26:23 -0800

    Array chunking

--- js/solution105.js
+++ js/solution105.js
@@ -8,15 +8,26 @@
 //[1, 2, 3], 1 => [[1], [2], [3]]
 //[1, 2, 3, 4], 5 => [1, 2, 3, 4]
 
-function chunkArray(array, size) {
+//for loop
+// function chunkArray(array, size) {
+//     let result = []
+//     for(value of array){
+//         let lastArray = result[result.length - 1]
+//         if(!lastArray || lastArray.length === size){
+//             result.push([value])
+//         }else{
+//             lastArray.push(value)
+//         }
+//     }
+//     return result
+// }
+
+//splice
+function chunkArray(array, size){
     let result = []
-    for(value of array){
-        let lastArray = result[result.length - 1]
-        if(!lastArray || lastArray.length === size){
-            result.push([value])
-        }else{
-            lastArray.push(value)
-        }
+    let arrayCopy = [...array]
+    while(arrayCopy.length > 0){
+        result.push(arrayCopy.splice(0, size))
     }
     return result
 }
