bcef1fc7437b430332661998d11c854f4b435c2c
My Computer <yokalen@gmail.com>
2023-03-14 10:01:43 -0700

    Get domain name from url

---
+++ js/solution116.js
@@ -0,0 +1,22 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet
+
+//url as a string
+//domain name as a string
+// http://github.com/carbonfive/raygun => github
+// http://www.zombie-bites.com => zombie-bites
+// https://www.cnet.com => cnet
+
+function domainName(url){
+    //drop http:// or https://
+    let result = url.split('//').slice(1).join('')
+    //split on . and return first element that is not www
+    return result = result.split('.').filter(el => el !== 'www')[0]
+}
+
+console.log(domainName("http://github.com/carbonfive/raygun"), "github")
+console.log(domainName("http://www.zombie-bites.com"), "zombie-bites")
+console.log(domainName("https://www.cnet.com"), "cnet")
