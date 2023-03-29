305e5ff554a75ac31e0c853ec6c418d40720f63c
My Computer <yokalen@gmail.com>
2023-03-28 20:11:18 -0700

    Final score

--- js/solution118.js
+++ js/solution118.js
@@ -4,13 +4,14 @@
 //[2, 2, 3, 0] => 1
 //[0, 0, 0, 4] => 1
 
-function getAvg(arr){
-  //add nums & divide by arr length
-  let result = arr.reduce((acc, c) => acc + c, 0) / arr.length;
-  //round down and return
-  return Math.floor(result);
-  
-}
+// function getAvg(arr){
+//   //add nums & divide by arr length
+//   let result = arr.reduce((acc, c) => acc + c, 0) / arr.length;
+//   //round down and return
+//   return Math.floor(result); 
+// }
+
+const getAvg = arr => Math.floor(arr.reduce((acc, c) => acc + c, 0) / arr.length);
 
 console.log(getAvg([2, 2, 2, 2]), 2)
 console.log(getAvg([2, 2, 3, 0]), 1)
