cabaf99b89097a86847254e1f1e2b86c2974df36
My Computer <yokalen@gmail.com>
2023-04-02 17:33:35 -0700

    Sum unique

--- js/solution119.js
+++ js/solution119.js
@@ -9,6 +9,8 @@ function repeats(arr){
   return result.reduce((acc, c) => acc + c, 0)
 }
 
+const repeats = arr => arr.filter(el => arr.indexOf(el) === arr.lastIndexOf(el)).reduce((acc, c) => acc + c, 0);
+
 console.log(repeats([4,5,7,5,4,8]),15)
 console.log(repeats([9, 10, 19, 13, 19, 13]),19)
 console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11]),12)
