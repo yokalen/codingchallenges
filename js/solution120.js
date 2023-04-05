0fe4baebe151c2b8455d18980423ba1b2d80029a
My Computer <yokalen@gmail.com>
2023-04-04 17:42:27 -0700

    Arr filtering

---
+++ js/solution120.js
@@ -0,0 +1,5 @@
+//arr of nums, another num
+//new arr with length of num, containing last even nums from arr
+//[1, 2, 3, 4, 5, 6, 7, 8, 9], 3 => [4, 6, 8]
+//[-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2 => [-8, 26]
+//[6, -25, 3, 7, 5, 5, 7, -3, 23], 1 => [6]
