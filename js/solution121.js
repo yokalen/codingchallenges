f6bbbcc9dc604b18e72222ccf41a3f612e44ad39
My Computer <yokalen@gmail.com>
2023-04-07 04:37:58 -0700

    .

---
+++ js/solution121.js
@@ -0,0 +1,6 @@
+//an array of arrays
+//number of possible unqiue arrays formed by taking 1 element from each sub array
+//[[1,2],[4],[5,6]] => 4
+//[[1,2],[4,4],[5,6,6]] => 4
+//[[1,2],[3,4],[5,6]] => 8
+//[[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]] => 72
