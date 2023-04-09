1864484042cedff45357f0fc46f9a5cabd7ec708
My Computer <yokalen@gmail.com>
2023-04-08 17:53:22 -0700

    Slice of a filtered arr

--- js/solution120.js
+++ js/solution120.js
@@ -4,12 +4,14 @@
 //[-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2 => [-8, 26]
 //[6, -25, 3, 7, 5, 5, 7, -3, 23], 1 => [6]
 
-function lastEvenNums(arr, num){
-    //filter the array for even nums
-    let result = arr.filter(el => el % 2 === 0);
-    //return last (num) values of the arr
-    return result.slice(-num);
-  }
+// function lastEvenNums(arr, num){
+//     //filter the array for even nums
+//     let result = arr.filter(el => el % 2 === 0);
+//     //return last (num) values of the arr
+//     return result.slice(-num);
+//   }
+
+const lastEvenNums = (arr, num) => arr.filter(el => el % 2 === 0).slice(-num);
 
 console.log(lastEvenNums([1, 2, 3, 4, 5, 6, 7, 8, 9], 3), "4, 6, 8")
 console.log(lastEvenNums([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2), "-8, 26")
