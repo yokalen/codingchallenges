beccf80f293e24bd36e3cfa00fbe33b1c1369ebb
My Computer <yokalen@gmail.com>
2023-04-23 15:00:17 -0700

    nth smallest element in an array

--- js/solution122.js
+++ js/solution122.js
@@ -4,11 +4,18 @@
 //[15, 20, 7, 10, 4, 3], 3 => 7
 //[177, 225, 243, -169, -12, -5, 2, 92], 5 => 92
 
-function nthSmallestNum(arr, n){
-    //sort the array in descending order
-    let arrSort = arr.sort((a, b) => b - a).slice(-n)
-    //return sorted array [-n]
-    return arrSort[0]
+// function nthSmallestNum(arr, n){
+//     //sort the array in descending order
+//     let arrSort = arr.sort((a, b) => b - a).slice(-n)
+//     //return sorted array [-n]
+//     return arrSort[0]
+//   }
+//other way to do that in reverse
+  function nthSmallestNum(arr, n){
+    //sort the array in ascending order
+    let arrSort = arr.sort((a, b) => a - b)
+    //return sorted array [n - 1]
+    return arrSort[n - 1]
   }
 
 console.log(nthSmallestNum([3,1,2], 2), 2);
