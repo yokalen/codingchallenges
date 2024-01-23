9ed9d82fbd65139726a82b0de6fec1415b4caf9b
My Computer <yokalen@gmail.com>
2024-01-23 09:10:37 -0800

    change approach

---
+++ js/solution123.js
@@ -0,0 +1,10 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+// "camelCasing" => "camel Casing" // "identifier" => "identifier" // "" => ""
+// split the string into an array of letters
+// map the array and if the letter is capitalized, add a space in front of it
+// join the array back into a string
+
+function solution(string) {
+    
+    return;
+  }
