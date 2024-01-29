854978b58d7973bdc871c56b5a3ea3013bfa455c
My Computer <yokalen@gmail.com>
2024-01-28 19:53:23 -0800

    problem def

---
+++ js/solution126.js
@@ -0,0 +1,5 @@
+//function zero() {} //function one() {}//function two() {} //function three() {} // function four() {} // function five() {} // function six() {} // function seven() {} // function eight() {} // function nine() {}  // function plus() {} // function minus() {} // function times() {} // function dividedBy() {}
+
+//write calculations using function and get the results
+
+//seven(times(five()))  returns 35 //four(plus(nine())) returns 13  //eight(minus(three()))  returns 5 //six(dividedBy(two()))  returns 3
