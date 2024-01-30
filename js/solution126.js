0ec5b385108c11155043173cfacf9174885864a1
My Computer <yokalen@gmail.com>
2024-01-29 19:58:03 -0800

    stopping point

--- js/solution126.js
+++ js/solution126.js
@@ -3,3 +3,56 @@
 //write calculations using function and get the results
 
 //seven(times(five()))  returns 35 //four(plus(nine())) returns 13  //eight(minus(three()))  returns 5 //six(dividedBy(two()))  returns 3
+
+function zero() {
+
+}
+function one() {
+
+}
+function two() {
+
+}
+function three() {
+
+}
+function four() {
+
+}
+function five() {
+    //a number 5
+    return 5;
+}
+function six() {
+
+}
+function seven() {
+    //a number 7
+    return 7;
+}
+function eight() {
+
+}
+function nine() {
+
+}
+
+function plus() {
+
+}
+function minus() {
+
+}
+function times() {
+    //multiply num 1 by num 2
+    
+}
+function dividedBy() {
+
+}
+
+//tests
+console.log(seven(times(five())), 35)
+console.log(four(plus(nine())), 13)
+console.log(six(dividedBy(two())), 3)
+
