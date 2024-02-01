062b085cf9dbb29bd278887e83601787a690d082
My Computer <yokalen@gmail.com>
2024-01-31 20:15:13 -0800

    second try

--- js/solution126.js
+++ js/solution126.js
@@ -4,27 +4,49 @@
 
 //seven(times(five()))  returns 35 //four(plus(nine())) returns 13  //eight(minus(three()))  returns 5 //six(dividedBy(two()))  returns 3
 
-function zero() {
-
+function zero(func) {
+    if(func){
+        return func(0);
+    }else{
+        return 0;}
 }
-function one() {
-
+function one(func) {
+    if(func){
+        return func(1);
+    }else{
+        return 1;}
 }
-function two() {
-
+function two(func) {
+    if(func){
+        return func(2);
+    }else{
+        return 2;}
 }
-function three() {
-
+function three(func) {
+    if(func){
+        return func(3);
+    }else{
+        return 3;}
 }
-function four() {
-
+function four(func) {
+    if(func){
+        return func(4);
+    }else{
+        return 4;}
 }
-function five() {
-    //a number 5
-    return 5;
+function five(func) {
+    if(func){
+        return func(5)
+    }else{
+        //a number 5
+        return 5;
+    } 
 }
-function six() {
-
+function six(func) {
+    if(func){
+        return func(6);
+    }else{
+        return 6;}
 }
 function seven(func) {
     //if function has an argument, return that function with 7 as an argument
@@ -35,18 +57,28 @@ function seven(func) {
         return 7;}
     
 }
-function eight() {
-
+function eight(func) {
+    if(func){
+        return func(8);
+    }else{
+        return 8;}
 }
-function nine() {
-
+function nine(func) {
+    if(func){
+        return func(9);
+    }else{
+        return 9;}
 }
 
-function plus() {
-
+function plus(val2) {
+    return function(val1){
+        return val1 + val2;
+    }
 }
-function minus() {
-
+function minus(val2) {
+    return function(val1){
+        return val1 - val2;
+    }
 }
 function times(val2) {
     //multiply num 1 by num 2
@@ -54,12 +86,14 @@ function times(val2) {
         return val1 * val2;
     }
 }
-function dividedBy() {
-
+function dividedBy(val2) {
+    return function(val1){
+        return val1 / val2;
+    }
 }
 
 //tests
 console.log(seven(times(five())), 35)
-// console.log(four(plus(nine())), 13)
-// console.log(six(dividedBy(two())), 3)
+console.log(four(plus(nine())), 13)
+console.log(six(dividedBy(two())), 3)
 
