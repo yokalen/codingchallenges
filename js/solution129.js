47ffd7f1ec125bd5a4238a7bd5053a8b26504651
My Computer <yokalen@gmail.com>
2024-04-02 20:38:28 -0700

    list filtering

---
+++ js/solution129.js
@@ -0,0 +1,6 @@
+//array of non-negative numbers and strings
+//return new arr without any of the strings
+//[1, 2, 'a', 'b'] => [1, 2]
+//[1, 'a', 'b', 0, 15] => [1, 0, 15]
+//[1, 2, 'ssaavvaaa', '1', '124', 123] => [1,2, 123]
+
