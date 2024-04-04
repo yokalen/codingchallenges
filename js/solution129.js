132be58f77ed72cc4fd743ee35543d47912e00c9
My Computer <yokalen@gmail.com>
2024-04-03 20:48:06 -0700

    Filtered list

--- js/solution129.js
+++ js/solution129.js
@@ -2,5 +2,9 @@
 //return new arr without any of the strings
 //[1, 2, 'a', 'b'] => [1, 2]
 //[1, 'a', 'b', 0, 15] => [1, 0, 15]
-//[1, 2, 'ssaavvaaa', '1', '124', 123] => [1,2, 123]
+//[1, 2, 'ssaavvaaa', '1', '124', 123] => [1, 2, 123]
 
+function filterList(arr){
+    let filtered = arr.filter(x => typeof x === "number")
+    return filtered;
+}
